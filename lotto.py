31c42b5677b17d7edeb223df7c4ee88629d882c6
Alice Caffyn <alice.caffyn@gmail.com>
2022-02-20 10:52:34 +0000

    lotto exercise set attempt

--- lotto.py
+++ lotto.py
@@ -1,3 +1,5 @@
 import random
-lotto_number = random.randrange(1,50)
+lotto_number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 17, 18, 29, 30}
+
+random.randrange(lotto_number)
 print(lotto_number)
