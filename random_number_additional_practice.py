af75e64d32e55aff3973427c8849406a5e9fe7ae
Alice Caffyn <alice.caffyn@gmail.com>
2022-02-23 14:37:20 +0000

    commit additional random exercise

---
+++ random_number_additional_practice.py
@@ -0,0 +1,15 @@
+import random
+# random_numbers = set(random.randint(1,39))
+# for number in random_numbers:
+#     random_numbers.add(number)
+# print(lotto_numbers)
+random_numbers = set()
+print(type(random_numbers))
+
+while len(random_numbers) < 11=:
+    number = random.randint(1,69)
+    random_numbers.add(number)
+print(random_numbers)
+
+random_numbers_sorted = sorted(random_numbers)
+print("Sorted random number are: ", random_numbers_sorted)
